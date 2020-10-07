e97c759a3c3acbde82ba478362e4f458f2fa57d8
skglw <skglvv@gmail.com>
2020-10-07 10:56:31 +0300

    completed

--- lab03-upgrade.lua
+++ lab03-upgrade.lua
@@ -3,9 +3,9 @@ lgi = require 'lgi'
 gtk = lgi.Gtk
 gdk = lgi.Gdk
 gtk.init()
+
 bld = gtk.Builder()
 bld:add_from_file('lab03-upgrade.glade')
-
 prov = gtk.CssProvider()
 prov:load_from_path('lab03-upgrade.css')
 
@@ -14,7 +14,6 @@ scr = gdk.Screen.get_default()
 ctx.add_provider_for_screen(scr, prov, gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
 ui = bld.objects
-
 ui.wnd.title = 'lab-03-zhuravleva-upgrade'
 ui.wnd.on_destroy = gtk.main_quit
 ui.wnd:show_all()
@@ -24,26 +23,29 @@ lbl = {ui.label4, ui.label3, ui.label2, ui.label1}
 
 function ui.entry:on_changed(...)
 
-for i=1,4 do
+for i = 1,4 do
 chb[i].active = false
-lbl[i].label = 0
+lbl[i].label = ""
 end
 
 num = tonumber(ui.entry.text)
 
-if num<16 then 
-if num>-1 then
+if num < 16 then 
+if num > -1 then
 
-for i=1,4 do
-max = math.pow(2, i)-1
-if max>=num then max=i break end
+for i = 1,4 do
+lbl[i].label = 0
 end
 
-	for i=1,max do
+for i = 1,4 do
+max = math.pow(2, i)-1
+if max >= num then max = i break end
+end
+
+	for i = 1,max do
 	flag = num%2
-	num= math.floor(num/2)
---if num == 1 then chb[i].active = true 
-		if flag==1 then 
+	num = math.floor(num / 2)
+		if flag == 1 then 
 			chb[i].active = true 
 			lbl[i].label = 1
 		end
